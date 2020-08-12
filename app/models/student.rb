df3aa08f3a29bab1359f14929790a1bfeda88576
hortencia c <hortenciaecisneros@gmail.com>
2020-08-12 13:32:10 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,22 @@
+ require 'pry'
+attr_accessible = :first_name,:boating_test,:all_instructors
+@@all = []
 class Student
 
+def intialize(first_name,boating_test,all_instructors)
+  @first_name = first_name
+  @boating_test = boating_test
+  @all_instructors = all_instructors
+  binding.pry
+  @@all << self
 end
+
+def self 
+  @@all
+end
+
+
+
+
+end
+# binding.pry
